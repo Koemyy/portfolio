aeef8c0cf162d423d520013cabfed1da8215ec6d
Koemy <francine.pns@gmail.com>
2022-10-23 12:03:02 -0300

    Start avatar section

---
+++ components/avatar.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import 
+interface avatar {
+    image: string
+}
+
+function Avatar() {
+    return(
+        <div className="">
+            
+        </div>
+    );
+}
+
+export default Avatar;
