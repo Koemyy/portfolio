bfbcadb91a0ed3d763ee2276507e9652a1729022
Koemy <francine.pns@gmail.com>
2022-10-23 12:29:07 -0300

    Finish avatar section

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/'
-
-interface avatar {
-    image: string
-}
+import Image from 'next/image'
+import me from '../public/me.png'
 
 function Avatar() {
     return(
-        <div className="">
-            
+        <div className="mx-auto relative bg-gradient-to-b from-pink-400 rounded-full w-80 h-80 mt-10 overflow-hidden">
+            <Image src={me} layout="fill" objectFit='cover'/>
         </div>
     );
 }
