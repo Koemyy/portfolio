b079e8bfa49862ca9a42f93a6c0740f65aab97f1
Koemy <francine.pns@gmail.com>
2022-10-23 12:29:25 -0300

    Add a little animation

--- components/icons.tsx
+++ components/icons.tsx
@@ -4,9 +4,9 @@ import {FiFigma} from 'react-icons/fi'
 function Icons() {
     return(
         <div className='text-5xl flex justify-center gap-16 py-3 text-gray-800'>
-           <a href=""><AiFillGithub /></a>
-           <a href=""><AiFillLinkedin /></a>
-           <a href=""><FiFigma /></a>
+           <a className='hover:text-pink-300 ease-in duration-200' href=""><AiFillGithub /></a>
+           <a className='hover:text-pink-300 ease-in duration-200' href=""><AiFillLinkedin /></a>
+           <a className='hover:text-pink-300 ease-in duration-200' href=""><FiFigma /></a>
         </div>
     );
 }
