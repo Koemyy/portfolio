18ea08226d325b00ac37782cfa4d987b43e51c60
Koemy <francine.pns@gmail.com>
2022-10-23 12:02:43 -0300

    Create navbar

---
+++ components/navBar.tsx
@@ -0,0 +1,25 @@
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+
+interface navBar {
+    title: string,
+    label: string,
+    href: string,
+}
+
+function Navbar({title, label, href}: navBar) {
+    return(
+        <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>{title}</h1>
+            <ul className='flex items-center'>
+                <li>
+                    <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
+                </li>
+                <li>
+                    <a className='bg-gradient-to-r from-pink-400 to-pink-300 text-white px-4 py-2 rounded-md ml-8' href={href}>{label}</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
