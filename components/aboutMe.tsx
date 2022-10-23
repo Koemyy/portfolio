9b3fa32357eb7ed7635d67c82a186f0d144b541f
Koemy <francine.pns@gmail.com>
2022-10-23 16:39:59 -0300

    Add a span tag

--- components/aboutMe.tsx
+++ components/aboutMe.tsx
@@ -9,7 +9,10 @@ function AboutMe({name, professional, about}: aboutMe) {
         <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-pink-400 font-medium'>{name}</h2>
             <h3 className='text-2xl py-2'>{professional}</h3>
-            <p className='text-md py-5 leading-8 text-gray-700'>{about}</p>
+            <div className="flex justify-center">
+                <p className='lg:w-96 text-md py-5 leading-8 text-gray-700'>{about}</p>
+            </div>
+            <span className="text-pink-400 font-medium">Nice to meet you!</span>
         </div>
     );
 }
