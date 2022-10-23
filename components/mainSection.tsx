2c4a72aba470717fb4b7db45e7ddf683702b9df2
Koemy <francine.pns@gmail.com>
2022-10-23 16:41:18 -0300

    Remove typo

--- components/mainSection.tsx
+++ components/mainSection.tsx
@@ -8,13 +8,12 @@ import ArtSection from './artSection';
 function MainSection() {
     return(
         <section className="min-h-screen">
-            <Navbar title="Hey there!" href='#' label='Resume'/>
+            <Navbar title="Welcome ;)" href='#' label='Resume'/>
             <AboutMe name="Francine Nogueira" professional="Front-end developer and UI/UX designer." about="Systems analysis and development student, 
-            I'm very interested in the front-end and UI/UX area, always looking to learn and understand the user's needs. Nice to meet you!"/>
+            I'm very interested in the front-end and UI/UX area, always looking to learn and understand the user's needs."/>
             <Icons/>
             <Avatar/>
             <ArtSection title='My Works' subtitle="Take a look at my projects in Figma and some sites that I've made!"/>
-            
         </section>
     );
 }
