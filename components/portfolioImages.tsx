a86ca3052c39555a4960d0ad9cafaea0b9f40274
Koemy <francine.pns@gmail.com>
2022-10-24 18:15:06 -0300

    Create images section for my projects

---
+++ components/portfolioImages.tsx
@@ -0,0 +1,22 @@
+import Image, { StaticImageData } from 'next/image'
+
+interface portfolioImages {
+    image: StaticImageData,
+}
+
+function PortfolioImages(props: portfolioImages) {
+    return(
+        <div className='lg:mx-5 lg:basis-1/3 border-2 border-pink-200 rounded-lg  hover:border-pink-300 ease-in duration-'>
+            <div className='cursor-pointer'>
+                <Image 
+                className='object-cover rounded-lg' 
+                src={props.image} 
+                width={150} 
+                height={100} 
+                layout="responsive"/>
+                </div>
+        </div>
+    );
+}
+
+export default PortfolioImages;
