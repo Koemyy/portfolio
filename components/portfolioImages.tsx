8458346db64aaa889de98ca0c9a951bdb333efb7
Koemy <francine.pns@gmail.com>
2022-10-25 14:54:32 -0300

    Add element easing duration

--- components/portfolioImages.tsx
+++ components/portfolioImages.tsx
@@ -6,7 +6,7 @@ interface portfolioImages {
 
 function PortfolioImages(props: portfolioImages) {
     return(
-        <div className='lg:mx-5 lg:basis-1/3 border-2 border-pink-200 rounded-lg  hover:border-pink-300 ease-in duration-'>
+        <div className='lg:mx-5 lg:basis-1/3 border-2 border-pink-200 rounded-lg  hover:border-pink-300 ease-in duration-300'>
             <div className='cursor-pointer'>
                 <Image 
                 className='object-cover rounded-lg' 
