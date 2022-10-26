0f78dc7480cb955aaad59abcc8fca603a5490c74
Koemy <francine.pns@gmail.com>
2022-10-26 15:24:18 -0300

    Add action handler

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,21 +1,18 @@
+import { MouseEventHandler } from 'react';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 
 interface navBar {
     title: string,
-    label: string,
-    href: string,
+    action: MouseEventHandler,
 }
 
-function Navbar({title, label, href}: navBar) {
+function Navbar({title, action}: navBar) {
     return(
         <nav className='py-10 mb-12 flex justify-between'>
             <h1 className='text-xl'>{title}</h1>
             <ul className='flex items-center'>
-                <li>
-                    <BsFillMoonStarsFill className='text-gray-700 cursor-pointer text-2xl'/>
-                </li>
-                <li>
-                    <a className='bg-gradient-to-r from-pink-400 to-pink-300 text-white px-4 py-2 rounded-md ml-8' href={href}>{label}</a>
+                <li hidden>
+                    <BsFillMoonStarsFill onClick={action} className='text-gray-700 cursor-pointer text-2xl'/>
                 </li>
             </ul>
         </nav>
