6e70f2e69cbc38bee5d68a3e5be654fc9bd0d835
Koemy <francine.pns@gmail.com>
2022-11-27 18:24:40 -0300

    Fix layout

--- components/portfolioImages.tsx
+++ components/portfolioImages.tsx
@@ -2,19 +2,22 @@ import Image, { StaticImageData } from 'next/image'
 
 interface portfolioImages {
     image: StaticImageData,
+    link: string | undefined,
 }
 
 function PortfolioImages(props: portfolioImages) {
     return(
         <div className='lg:mx-5 lg:basis-1/3 border-2 border-pink-200 rounded-lg  hover:border-pink-300 ease-in duration-300'>
-            <div className='cursor-pointer'>
-                <Image 
-                className='object-cover rounded-lg' 
-                src={props.image} 
-                width={150} 
-                height={100} 
-                layout="responsive"/>
-                </div>
+            <a href={props.link} target="_blank" rel="noreferrer">
+                <div className='cursor-pointer'>
+                    <Image
+                    className='object-cover rounded-lg'
+                    src={props.image}
+                    width={150}
+                    height={100}
+                    layout="responsive"/>
+                    </div>
+            </a>
         </div>
     );
 }
