263c6385f6fa45e85740028226b2b867bb664ca3
Koemy <francine.pns@gmail.com>
2022-11-27 18:24:02 -0300

    Add links and fix security problems

--- components/icons.tsx
+++ components/icons.tsx
@@ -4,9 +4,9 @@ import {FiFigma} from 'react-icons/fi'
 function Icons() {
     return(
         <div className='text-5xl flex justify-center gap-16 py-3 text-gray-800'>
-           <a className='hover:text-pink-300 ease-in duration-200' href=""><AiFillGithub /></a>
-           <a className='hover:text-pink-300 ease-in duration-200' href=""><AiFillLinkedin /></a>
-           <a className='hover:text-pink-300 ease-in duration-200' href=""><FiFigma /></a>
+           <a className='hover:text-pink-300 ease-in duration-200' href="https://github.com/Koemyy" target="_blank" rel="noreferrer"><AiFillGithub /></a>
+           <a className='hover:text-pink-300 ease-in duration-200' href="https://www.linkedin.com/in/francinepns/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
+           <a className='hover:text-pink-300 ease-in duration-200' href="https://www.figma.com/@koemy" target="_blank" rel="noreferrer"><FiFigma /></a>
         </div>
     );
 }
