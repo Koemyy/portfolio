00105505457b168e1f018e8e78a9790f62347440
Koemy <francine.pns@gmail.com>
2023-10-06 11:42:20 -0300

    Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import '../styles/globals.css'
-import type {AppProps} from 'next/app'
-import GoogleTagManager from "../components/GTM/GoogleTagManager";
-import {Head} from 'next/document';
+import type { AppProps } from 'next/app'
 
-function MyApp({Component, pageProps}: AppProps) {
-    return (
-        <>
-            <Head>
-                <GoogleTagManager/>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
